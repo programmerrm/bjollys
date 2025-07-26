64232af630c3ba5c778fb23da24dd6f51c23dc52
programmerrm <programmerwebrm@gmail.com>
2025-07-26 11:58:49 +0600

    gbh

--- client/app/utils/api.ts
+++ client/app/utils/api.ts
@@ -1,2 +1,2 @@
-export const MEDIA_URL = import.meta.env.VITE_MEDIA_URL;
-export const SERVER_URL = import.meta.env.VITE_SERVER_API_URL;
+export const MEDIA_URL = import.meta.env.VITE_MEDIA_URL || "https://api.bjollys.net/";
+export const SERVER_URL = import.meta.env.VITE_SERVER_API_URL || "https://api.bjollys.net/api/v1";
